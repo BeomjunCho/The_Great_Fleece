8007c51bda87ace103fb9ff33f7edd77ae3ca206
BeomjunCho <chobj555@gmail.com>
2025-01-30 17:25:10 -0800

    cutscene and nav mesh
    Completed cutscene
    Started player controller and nav mesh Ai things

---
+++ TheGreatFleece/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                Debug.Log($"Hit: {hitInfo.point}");
+
+                _agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
